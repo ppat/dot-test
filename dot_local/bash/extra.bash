1e75875a7497d19c6bb05d84f7dc287d406d1010
peter pathirana <peter@latent.io>
2025-05-26 21:46:44 +0000

    fix

--- dot_local/bash/extra.bash
+++ dot_local/bash/extra.bash
@@ -1,19 +1,14 @@
 # Additional Bash customizations
 
-# Enable kubectl completion
-if command -v kubectl &> /dev/null; then
-  source <(kubectl completion bash)
-  complete -F __start_kubectl k
-fi
-
-# Enable helm completion
-if command -v helm &> /dev/null; then
-  source <(helm completion bash)
-fi
-
 # # Better history command defaults
 # export HISTSIZE=10000
 # export HISTFILESIZE=20000
 # export HISTCONTROL=ignoreboth:erasedups
 # export HISTTIMEFORMAT="%F %T "
-shopt -s histappend
+# shopt -s histappend
+
+if [[ -f $HOME/.krew/bin/kubectl-cnpg ]]; then
+  ln -sf $HOME/.krew/bin/kubectl-cnpg $HOME/.local/bin/cnpg
+  source <($HOME/.local/bin/cnpg completion bash)
+fi
+
